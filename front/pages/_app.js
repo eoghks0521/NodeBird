9eaee92e305e2968843e7f8143e6afc32b65bc99
daehwan <eoghks0521@naver.com>
2020-06-20 18:13:36 +0900

    Add redux-saga

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -3,19 +3,21 @@ import Head from 'next/head';
 import PropTypes from 'prop-types';
 import withRedux from 'next-redux-wrapper';
 import AppLayout from '../components/AppLayout';
-import { applyMiddleware, compose, createStore } from 'redux';
-import { Provider } from 'react-redux';
+import {applyMiddleware, compose, createStore} from 'redux';
+import {Provider} from 'react-redux';
 import reducer from '../reducers';
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from "../sagas";
 
-const NodeBird = ({ Component, store }) => {
+const NodeBird = ({Component, store}) => {
 	return (
 		<Provider store={store}>
 			<Head>
 				<title>NodeBird</title>
-				<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.16.2/antd.css" />
+				<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.16.2/antd.css"/>
 			</Head>
 			<AppLayout>
-				<Component />
+				<Component/>
 			</AppLayout>
 		</Provider>
 	);
@@ -27,11 +29,16 @@ NodeBird.propTypes = {
 };
 
 export default withRedux((initialState, options) => {
-	const middlewares = [];
-	const enhancer = compose(
-		applyMiddleware(...middlewares),
-		!options.isServer && typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== 'undefined' ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f,
-	);
+	const sagaMiddleware = createSagaMiddleware();
+	const middlewares = [sagaMiddleware];
+	const enhancer = process.env.NODE_ENV === 'production'
+		? compose(
+			applyMiddleware(...middlewares),)
+		: compose(
+			applyMiddleware(...middlewares),
+			!options.isServer && typeof window.__REDUX_DEVTOOLS_EXTENSION__ !== 'undefined' ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f,
+		);
 	const store = createStore(reducer, initialState, enhancer);
+	sagaMiddleware.run(rootSaga);
 	return store;
 })(NodeBird);
